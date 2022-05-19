9c41b8d7782a343124b34398e2cccf9108644313
Adam Gariba <agariba101@gmail.com>
2022-05-18 21:09:22 -0400

    application inherits from click MultiCommand interface

--- atem/cli.py
+++ atem/cli.py
@@ -1,6 +1,29 @@
+import os
+
 import click
 
-@click.command()
+cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
+
+
+class ComplexCLI(click.MultiCommand):
+    def list_commands(self, ctx):
+        commands = []
+        for filename in os.listdir(cmd_folder):
+            if filename.endswith(".py") and filename.startswith("cmd_"):
+                commands.append(filename[4:-3])
+        commands.sort()
+        return commands
+
+    def get_command(self, ctx, name):
+        try:
+            mod = __import__(f"atem.commands.cmd_{name}", None, None, ["cli"])
+        except ImportError:
+            return
+        return mod.cli
+
+
+
+@click.command(cls=ComplexCLI)
 def cli():
-    """Example script."""
-    click.echo('Hello, I\'m Atem!')
+    """Hello! I am Atem!"""
+    pass
