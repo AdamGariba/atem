7077ab418e0bdfcf67ca3a4eaa1128a70ad45c6d
Adam Gariba <agariba101@gmail.com>
2022-06-27 15:40:35 -0400

    add access to database name

--- atem/config.py
+++ atem/config.py
@@ -7,3 +7,4 @@ load_dotenv()
 # Quotes API
 QUOTE_API = os.environ.get("QUOTE_API", "<your-api-key>")
 QUOTE_RANDOM = os.environ.get("QUOTE_RANDOM", "<your-api-endpoint>")
+DATABASE = os.environ.get("DATABASE", "<database-filepath>")
